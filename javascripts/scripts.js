f25f5af73baeb41f8027ed8c6a0a27b93a38683e
Nick Kugaevsky <nick@kugaevsky.ru>
2012-08-30 23:04:07 +0400

    Github and coderwall via proudify

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,13 +1,21 @@
 (function() {
 
   $(function() {
-    return $('.fame-toggler .btn').click(function() {
+    $('.fame-toggler .btn').click(function() {
       if ($(this).val() === '0') {
         $('.hall-of-fame').removeClass('in');
       }
       if ($(this).val() === '1') {
         return $('.hall-of-fame').addClass('in');
       }
+    });
+    $('#coderwall-badges').proudify({
+      'service': 'coderwall',
+      'username': 'kugaevsky'
+    });
+    return $('#github-projects').proudify({
+      'service': 'github',
+      'username': 'kugaevsky'
     });
   });
 
--- source/javascripts/scripts.coffee
+++ source/javascripts/scripts.coffee
@@ -2,3 +2,11 @@ $ ->
   $('.fame-toggler .btn').click ->
     $('.hall-of-fame').removeClass('in') if $(@).val() is '0'
     $('.hall-of-fame').addClass('in') if $(@).val() is '1'
+
+  $('#coderwall-badges').proudify
+    'service': 'coderwall'
+    'username': 'kugaevsky'
+
+  $('#github-projects').proudify
+    'service': 'github'
+    'username': 'kugaevsky'
