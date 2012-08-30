cbcf0c2a3988ee599f645fb768e341567a888280
Nick Kugaevsky <nick@kugaevsky.ru>
2012-08-30 23:27:56 +0400

    Clean up

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,31 +10,10 @@ set :keep_releases, 5
 set :user, "u42828"
 set :use_sudo, false
 
-role :web, "u42828.ssh.masterhost.ru"                          # Your HTTP server, Apache/etc
-role :app, "u42828.ssh.masterhost.ru"                          # This may be the same as your `Web` server
+role :web, "u42828.ssh.masterhost.ru"                   # Your HTTP server, Apache/etc
+role :app, "u42828.ssh.masterhost.ru"                   # This may be the same as your `Web` server
 role :db,  "u42828.ssh.masterhost.ru", :primary => true # This is where Rails migrations will run
 
 # Hooks
 after "deploy:update", "deploy:cleanup"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
-
-# namespace :deploy do
-#   task :minify, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
